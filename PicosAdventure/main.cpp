c4f8ddfab3e16e74c502c45a682da545d19ef31b
Joan Mora Guiard <joan.mora@upf.edu>
2014-01-31 12:33:44 +0100

    Added the SystemClass, which has the purpose of creating a system window and displaying it. It also has the role of controlling the application's life loop

--- PicosAdventure/main.cpp
+++ PicosAdventure/main.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+#include "systemClass.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, //The handle of the application's current instance
 					HINSTANCE prevInstance,	//The handle to previous instance of the application. According to MSDN is always NULL.
@@ -6,6 +6,26 @@ int WINAPI wWinMain(HINSTANCE hInstance, //The handle of the application's curre
 									//the GetCommandLine function.
 					int cmdShow) //And ID that specifies how the window should be sown
 {
+	SystemClass* system; // Pointer ti the system object.
+	
+	// Create the system object. If it is not created, exit.
+	system = new SystemClass();
+	if(!system)
+	{
+		MessageBox(NULL, L"Could not create the SystemClass", L"Main - Error", MB_ICONERROR | MB_OK);
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	if(system->setup())
+	{
+		system->run();
+	}
+
+	// When done, destroy the system object and release.
+	system->destroy();
+	delete system;
+	system = 0;
 
 	return 0;
 }
