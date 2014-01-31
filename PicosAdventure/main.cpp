9055157fbd3b974e499bb807aed54b3eaf8ec627
Joan Mora Guiard <joan.mora@upf.edu>
2014-01-31 12:21:49 +0100

    Creating full directory structure and project/git filters

---
+++ PicosAdventure/main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, //The handle of the application's current instance
+					HINSTANCE prevInstance,	//The handle to previous instance of the application. According to MSDN is always NULL.
+					LPWSTR cmdLine,	//The command line for the application without program's name. To retrieve the entire command line, use
+									//the GetCommandLine function.
+					int cmdShow) //And ID that specifies how the window should be sown
+{
+
+	return 0;
+}
